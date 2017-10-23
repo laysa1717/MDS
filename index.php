7ba671b53818fbed8d9c7341106dba91ddaabf44
laysa1717 <laysa.santos919@gmail.com>
2017-10-23 00:26:36 -0200

    primeiro commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+      <div class="cabecalho">
+        <div class="isolamentoCabecalho">
+          <div   class="menu">
+       <div class="item">
+         <a href="">Home</a>
+       </div>
+       <div class="item">
+         <a href="">Serviços</a>
+       </div>
+       <div class="item">
+         <a href="faleconosco.php">Fale Conosco</a>
+       </div>
+     </div>
+        </div>
+      </div>
+      <div class="banner">
+        <div class="isolamentoBanner">
+
+          <div class="bannerInformacoes">
+          <table>
+            <tr>
+              <td class="td" >ligue agora</td>
+
+            </tr>
+            <tr>
+              <td class="comentarioAbaixo">E faça seu orçamento</td>
+            </tr>
+            <tr>
+              <td class="telefoneBanner">
+                (11)4707-8069
+              </td>
+            </tr>
+          </table>
+        </div>
+
+        </div>
+      </div>
+      <div class="servicos">
+        <div class="isolamentoServicos">
+
+        </div>
+      </div>
+      <div class="quemsomos">
+        <div class="isolamentoQuemsomos">
+
+        </div>
+      </div>
+
+      <div class="faleconosco">
+        <div class="isolamentoFaleConosco">
+
+        </div>
+      </div>
+      <div class="rodape">
+        <div class="isolamentoRodape">
+
+        </div>
+      </div>
+
+
+  </body>
+</html>
